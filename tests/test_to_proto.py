377807681d6215c10eb275f8ad65f795550cf534
ATTY Lionel <lionel.atty@365talents.com>
2021-01-29 17:55:05 +0100

    doc: add some doc about useful (potentially) links

--- tests/test_to_proto.py
+++ tests/test_to_proto.py
@@ -1,17 +1,43 @@
+from dataclasses import dataclass
+from typing import Optional, Type
+
+from parametrization import Parametrization
 from pydantic import BaseModel
+from pydantic import create_model as create_pydantic_model
 
 from pydantic2protobuf.services.to_proto import pydantic_to_proto
+from tests.tools.parametrization_case import IParametrizationCase
 
 
-class Model(BaseModel):
-    a: float
-    b: int = 10
+@dataclass
+class ParametrizationCase(IParametrizationCase):
+    pydantic_model: Type[BaseModel]
+    expected_proto_msg: str
 
 
-def test_pydantic_to_proto():
-    result_proto_msg = pydantic_to_proto(Model)
-    expected_proto_msg = """message Model {
+@Parametrization.autodetect_parameters()
+@IParametrizationCase.case(
+    ParametrizationCase(
+        "Pydantic model with basic types",
+        create_pydantic_model("Model", a=(float, ...), b=(int, 10), __base__=BaseModel),
+        """message Model {
     float a = 1;
     int32 b = 2;
-}"""
-    assert result_proto_msg == expected_proto_msg
+}""",
+    )
+)
+@IParametrizationCase.case(
+    ParametrizationCase(
+        "Pydantic model with optional fields",
+        create_pydantic_model(
+            "InfoSentenceRequest", text=(Optional[str], None), skill_id=(Optional[int], None), __base__=BaseModel
+        ),
+        """message InfoSentenceRequest {
+    google.protobuf.StringValue text = 1;
+    google.protobuf.UInt32Value skill_id = 2;
+}""",
+    )
+)
+def test_pydantic_model_to_proto_msg(pydantic_model, expected_proto_msg):
+    generated_proto_msg = pydantic_to_proto(pydantic_model)
+    assert generated_proto_msg == expected_proto_msg
