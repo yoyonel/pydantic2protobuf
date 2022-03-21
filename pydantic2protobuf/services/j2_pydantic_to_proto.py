258366691af5a4968ad4e9d8b8eb5c001743ecd4
ATTY Lionel <latty@unowhy.com>
2022-03-21 23:30:55 +0100

    refacto: transform direct stringification into data(class) results
    Signed-off-by: ATTY Lionel <latty@unowhy.com>

---
+++ pydantic2protobuf/services/j2_pydantic_to_proto.py
@@ -0,0 +1,64 @@
+from dataclasses import dataclass
+from typing import Dict, List, Optional
+
+from pydantic.fields import ModelField
+from pydantic.main import BaseModel, ModelMetaclass
+
+from pydantic2protobuf.tools.from_pydantic import (
+    PythonToGoogleProtoBufTypes,
+    PythonToProtoBufTypes,
+    extract_proto_fields,
+    is_type_iterable,
+)
+
+
+@dataclass
+class FieldDefinition:
+    proto_message: Optional[str]
+    disable_rpc: Optional[bool]
+    is_iterable: Optional[bool]
+    is_unsigned: Optional[bool]
+    type_translated: str
+    field_name: str
+    field_number: Optional[int]
+
+
+@dataclass
+class MessageDefinition:
+    name: str
+    fields: list[FieldDefinition]
+
+
+def translate_type(field: ModelField, proto_fields: dict) -> str:
+    field_allow_none = bool(proto_fields.get("allow_none") and field.allow_none)
+    map_for_types = (PythonToProtoBufTypes, PythonToGoogleProtoBufTypes)[field_allow_none]
+    return map_for_types.get(field.type_) or field.type_.__qualname__
+
+
+def gen_field_definition(field: ModelField, field_properties: Dict, enumerate_number: int) -> FieldDefinition:
+    proto_fields = extract_proto_fields(field_properties, default_number=enumerate_number)
+    return FieldDefinition(
+        proto_message=proto_fields.get("protobuf_message"),
+        disable_rpc=proto_fields.get("disable_rpc"),
+        is_iterable=is_type_iterable(field.outer_type_),
+        is_unsigned=proto_fields.get("is_unsigned"),
+        type_translated=translate_type(field, proto_fields),
+        field_name=field.name,
+        field_number=proto_fields.get("number"),
+    )
+
+
+def gen_fields_definitions(base_model: BaseModel) -> List[FieldDefinition]:
+    cls_properties = base_model.schema()["properties"]
+    return [
+        gen_field_definition(field, cls_properties[field.name], default_number)
+        for default_number, field in enumerate(base_model.__fields__.values(), start=1)
+    ]
+
+
+def gen_message_definition(base_model) -> MessageDefinition:
+    pydantic_base_model: BaseModel = base_model
+    pydantic_model_meta_class: ModelMetaclass = base_model
+    return MessageDefinition(
+        name=pydantic_model_meta_class.__qualname__, fields=gen_fields_definitions(pydantic_base_model)
+    )
